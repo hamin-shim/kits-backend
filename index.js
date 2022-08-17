af500bf5847f653a5b6bcb28bf7d2d800bcb9eda
Jinho Kim <79720217+real-lake050317@users.noreply.github.com>
2022-08-17 19:33:48 +0900

    Added basic mongoose connection module

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import cors from 'cors';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import compression from 'compression';
+import mongoose from 'mongoose';
 
 const app = express();
 dotenv.config();
@@ -17,3 +18,12 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(compression());
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Connected to Mongo DB");
+    } catch (err) {
+        throw err;
+    }
+}
