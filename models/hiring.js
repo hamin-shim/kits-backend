7960ccf13b647e79a1822222c6a9a4974f7d4034
KIM <km0192@naver.com>
2022-09-14 08:43:02 +0900

    feat: hiring schema

---
+++ models/hiring.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import CompanySchema from "./companies";
+import JobTypeSchema from "./job_types";
+import AddressSchema from "./address";
+import LocationSchema from "./location";
+
+const { Schema } = mongoose;
+
+const HiringSchema = new mongoose.Schema({
+    company:[CompanySchema],
+    job_type:[JobTypeSchema],
+    salary:{
+        type:String,
+        required:true
+    },
+    due_date:new Date(),
+    detail:{
+        type:String,
+        default:""
+    },
+    other_text: String,
+    workplace_address:[AddressSchema],
+    workplace_location:[LocationSchema],
+    is_hiring:{
+        type:Boolean,
+        required:true
+    }
+})
+
+export default mongoose.model("Hiring", HiringSchema);
