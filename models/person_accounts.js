ff89e378b7ac0e55c79d4ad51ed7802b7da6f10c
KIM <km0192@naver.com>
2022-09-14 00:49:00 +0900

    feat: add person_accounts

---
+++ models/person_accounts.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+import AddressSchema from "./address";
+import LocationSchema from "./location";
+
+const { Schema } = mongoose;
+
+const PersonAccountsSchema = new mongoose.Schema({
+    account: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    contact_number: {
+        type: String,
+        required: true
+    },
+    location: [LocationSchema],
+    address : [AddressSchema],
+    password_update_at: new Date(),
+    social : {
+        type : {
+            type:String,
+            required: true
+        },
+        social_id : {
+            type: String,
+            required: true
+        }
+    },
+    authorized: {
+        type: Boolean,
+        required: true
+    }
+});
+
+export default mongoose.model("PersonAccounts", PersonAccountsSchema);
